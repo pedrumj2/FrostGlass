eb6f0802dde9f75857659818039b8eb11953fa16
Pedrum <pedrum.jalali@gmail.com>
2017-01-07 21:36:17 -0500

    Revert "testing with 3d translate"
    This reverts commit 96768f0fdcfa12552413a3d9f7211a2f7e28ce87.

--- imgAlign.js
+++ imgAlign.js
@@ -14,7 +14,6 @@ var imgAlign = function(__idImg, __idParent, __idParentImg){
         parentImg: null, 
         left : -1,  
         top : -1, 
-        topParent : - 1, 
         idImg : __idImg, 
         idParent : __idParent, 
         idParentImg : __idParentImg, 
@@ -27,32 +26,21 @@ var imgAlign = function(__idImg, __idParent, __idParentImg){
 
         set_handlers : function (){
             window.addEventListener("resize", INNER.resize);
-          //  window.addEventListener("scroll", INNER.reposition);
         }, 
 
 
         get_position : function(){
-         
-           // INNER.left = INNER.img.getBoundingClientRect().left;
-           // INNER.top = INNER.img.getBoundingClientRect().top;
+            INNER.img.style.marginLeft = 0;
+             INNER.img.style.marginTop= 0;
+            INNER.left = INNER.img.getBoundingClientRect().left;
+            INNER.top = INNER.img.getBoundingClientRect().top;
         }, 
         resize : function(){
             INNER.get_position();
             INNER.set_width();
             INNER.set_height();
-             INNER.left = INNER.img.getBoundingClientRect().left;
-                INNER.top = INNER.img.getBoundingClientRect().top;
-                 INNER.topParent = INNER.parent.getBoundingClientRect().top;
-          //  INNER.set_left();
-           // INNER.set_top();
-             INNER.set_top_left();
-        }, 
-
-        reposition : function(){
-            INNER.get_position();
-           // INNER.set_left();
-           // INNER.set_top();
-           INNER.set_top_left();
+            INNER.set_left();
+            INNER.set_top();
         }, 
         set_width : function(){
             var _widthParent  = INNER.parent.offsetWidth;
@@ -72,45 +60,24 @@ var imgAlign = function(__idImg, __idParent, __idParentImg){
 
             var _imgLeft = INNER.left;
             var _x = (_parentImgLeft-_imgLeft )/_widthParent*100;
-          // var _x = (_parentImgLeft-_imgLeft );
             var _y = _x.toString() + "%";;
             INNER.img.style.marginLeft = _x.toString() + "%";
-            //INNER.img.style.transform = "translate3d("+_x.toString()+", 0, 0)";
         }, 
          set_top : function(){
-           
             var _widthParent  = INNER.parent.offsetWidth;
             var _parentImgTop = INNER.parentImg.getBoundingClientRect().top;
             var _imgTop = INNER.top;
             var _x = (_parentImgTop-_imgTop )/_widthParent*100;
-            // var _x = (_parentImgTop-_imgTop );///_widthParent*100;
             var _y = _x.toString() + "%";;
             INNER.img.style.marginTop = _x.toString() + "%";
-           //  INNER.img.style.transform = "translate3d(0, "+_x.toString()+", 0, 0)";
            
-        }, 
-
-        set_top_left: function(){
-              var _disp =INNER.parent.getBoundingClientRect().top - INNER.topParent;
-            var _parentImgLeft = INNER.parentImg.getBoundingClientRect().left;
-
-            var _imgLeft = INNER.left;
-           var _x = (_parentImgLeft-_imgLeft );
-            var _parentImgTop = INNER.parentImg.getBoundingClientRect().top;
-            var _imgTop = INNER.top;
-             var _y = (_parentImgTop-_imgTop-_disp );///_widthParent*100;
-
-            //INNER.img.style.marginLeft = _x.toString() + "%";
-            var z= "translate3d("+_x.toString()+", "+ _y.toString()+", 0)";
-            INNER.img.style.transform = "translate3d("+_x.toString()+"px, "+ _y.toString()+"px, 0)";
-        
         }, 
 
         set_objects : function(){
                 INNER.img = document.getElementById(__idImg);
                 INNER.parent = document.getElementById(__idParent);
                 INNER.parentImg = document.getElementById(__idParentImg);
-               
+                
         }, 
 
     };
